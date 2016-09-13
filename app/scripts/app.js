bf6583a7e086fd4d0d9368a17a20754d943c6e1f
Andrew Scobie <andrew.scobie@gmail.com>
2016-09-13 17:58:28 +1200

    adding adal angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,26 @@
 (function () {
     'use strict';
     
-    var _templateBase = './scripts';
-    console.log("app/js loaded");
+    const _templateBase = './scripts';
 
-    angular.module('app', [
-        'ngRoute',
-        'ngMaterial',
-        'ngAnimate'
-    ])
-    .config(['$routeProvider', function ($routeProvider) {
-            console.log("$routeProvider: ", $routeProvider);
+    angular.module('app', ['ngRoute', 'AdalAngular', 'ngMaterial', 'ngAnimate'])
+    .config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
             $routeProvider.when('/', {
                 templateUrl: _templateBase + '/subscription/templates/subscription-list.html' ,
                 controller: 'subscriptionController',
                 controllerAs: '_ctrl'
             });
+
             $routeProvider.otherwise({ redirectTo: '/' });
+
+            adalProvider.init({
+                instance: 'https://login.microsoftonline.com/', 
+                tenant: 'microsoft.onmicrosoft.com',
+                clientId: '36c32244-348e-4f3d-945d-9158435fcd48',
+                extraQueryParameter: 'nux=1',
+                //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+            }, $httpProvider);
         }
     ]);
+
 })();
--- app/scripts/subscription/subscriptionService.js
+++ app/scripts/subscription/subscriptionService.js
@@ -2,6 +2,9 @@
 (function () {
     'use strict';
     
+    const msRestAzure = require('ms-rest-azure');
+    const batchManagementClient = require('azure-arm-batch');
+
     angular.module('app')
         .service('subscriptionService', ['$q', SubscriptionService]);
     
@@ -15,6 +18,36 @@
         function listSubscriptions() {
             console.log("SubscriptionService.listSubscriptions");
             var deferred = $q.defer();
+
+
+
+/*
+            var subscriptionClient = new SubscriptionManagementClient(credentials);
+            setTimeout(function () {
+                console.log('Fetching subscriptions as the environment variable was not set.')
+                subscriptionClient.subscriptions.list(function (err, subscriptionList) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    
+                    subscriptionId = subscriptionList[0].subscriptionId;
+                    console.log('\nSetting \'' + subscriptionId + '\' as the current subscription...');
+                    
+                    //return callback(null);
+                });
+            }, 5000);
+
+            msRestAzure.interactiveLogin(function(err, credentials) {
+                var client = new batchManagementClient(credentials, 'your-subscription-id');
+                client.account.list(rgName, function(err, result, request, response) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    
+                    console.log(result);
+                });
+            });
+*/
             deferred.resolve([
                 {id: "1", name: "andrew1-fromlist", selected: false },
                 {id: "2", name: "andrew2-fromlist", selected: false  }
